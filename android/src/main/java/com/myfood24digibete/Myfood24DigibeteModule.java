69cc7797ba49ca69782ca1dce1ac1b0dcbcfcad6
Dan James <dan@coherent.net>
2022-12-16 16:54:17 +0000

    Android build

--- android/src/main/java/com/myfood24digibete/Myfood24DigibeteModule.java
+++ android/src/main/java/com/myfood24digibete/Myfood24DigibeteModule.java
@@ -8,12 +8,17 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import net.coherent.myfood24sdklib.Myfood24_StartActivity;
 @ReactModule(name = Myfood24DigibeteModule.NAME)
 public class Myfood24DigibeteModule extends ReactContextBaseJavaModule {
   public static final String NAME = "Myfood24Digibete";
-
+  ReactApplicationContext reactContext;
   public Myfood24DigibeteModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    this.reactContext = reactContext;
   }
 
   @Override
@@ -29,4 +34,16 @@ public class Myfood24DigibeteModule extends ReactContextBaseJavaModule {
   public void multiply(double a, double b, Promise promise) {
     promise.resolve(a * b);
   }
+  @ReactMethod
+  public void divide(double a, double b, Promise promise) {
+    promise.resolve(a / b);
+  }
+  @ReactMethod
+  public void test() {
+    Activity currentActivity = getCurrentActivity();
+    Intent intent = new Intent(currentActivity, Myfood24_StartActivity.class);
+    assert currentActivity != null;
+    currentActivity.startActivity(intent);
+  }
+
 }
